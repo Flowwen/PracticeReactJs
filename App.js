74134e86ff4e368be0e10e6041566de391d201a0
Flowwen <pprathamesh1906@gmail.com>
2023-06-10 18:12:32 +0530

    Nesting in React.

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-const heading = React.createElement("h1",
-    {
-        id: "heading",
-    }, "Hello world from React!");
 
-    console.log(heading);
+const parent = React.createElement("div", { id: "parent" },
+               React.createElement("div", { id: "child" },
+               React.createElement("h1", { id: "heading1" }, "I'm a h1 tag"))
+);
+
+
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 console.log(root);
 
-root.render(heading);
+root.render(parent);
