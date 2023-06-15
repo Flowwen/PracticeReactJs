f22b6401f9b6fcbd857adffbf2e0d867ca55b58c
Flowwen <pprathamesh1906@gmail.com>
2023-06-15 20:26:05 +0530

    Formatting with prettier

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
-
 import React from "react";
-import ReactDOM  from "react-dom/client"; 
+import ReactDOM from "react-dom/client";
 
-const jsxHeading = (<h1 id="heading" className="head">Namaste React </h1>)
+const jsxHeading = (
+  <h1 id="heading" className="head">
+    Namaste React using JSX 🚀
+  </h1>
+);
 
 const rootElem = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElem);
