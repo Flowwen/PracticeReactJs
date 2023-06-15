456cf0864f270611e914bc6cc3486684ec9cc3f1
Flowwen <pprathamesh1906@gmail.com>
2023-06-15 22:09:53 +0530

    Component Composition implemented.

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = (
-  <h1 id="heading" className="head">
-    Namaste React using JSX 🚀
-  </h1>
-);
+const TitleComponent = () => {
+  return (
+    <h1 id="heading" className="head">
+      Namaste React using JSX 🚀
+    </h1>
+  );
+};
 
 //React functional Component.
 
 const HeadingComponent = () => (
   <div id="container">
+    <TitleComponent />
     <h1 id="heading">Namaste React from a functional Component 🙏</h1>
   </div>
 );
