ca17054f0075f0dc862d06bf7163e4f8da96ac5e
Flowwen <pprathamesh1906@gmail.com>
2023-07-04 09:18:55 +0530

    Body and Restaurant card created.

--- App.js
+++ App.js
@@ -40,10 +40,43 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/h4yqggh2menundndcrmc"
+      />
+      <h3>Santosham</h3>
+      <h4>Dosa, South Indian, Asian </h4>
+      <h4>3.7 stars</h4>
+      <h4>22 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restaurant-container">
+        <RestaurantCard />  
+        <RestaurantCard />  
+        <RestaurantCard />  
+        <RestaurantCard />  
+        <RestaurantCard />  
+        <RestaurantCard />  
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
